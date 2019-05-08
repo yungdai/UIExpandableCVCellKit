a4c87b8f1709ff836e83a3d4c2ffbd74cf8a4d9e
Yung Dai <yungchidai@gmail.com>
2019-05-08 11:35:31 -0400

    Initial Commit

---
+++ UIExpandableCVCellKit/UIExpandableCVCellKit.h
@@ -0,0 +1,19 @@
+//
+//  UIExpandableCVCellKit.h
+//  UIExpandableCVCellKit
+//
+//  Created by Yung Dai on 2019-05-08.
+//  Copyright © 2019 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIExpandableCVCellKit.
+FOUNDATION_EXPORT double UIExpandableCVCellKitVersionNumber;
+
+//! Project version string for UIExpandableCVCellKit.
+FOUNDATION_EXPORT const unsigned char UIExpandableCVCellKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIExpandableCVCellKit/PublicHeader.h>
+
+
